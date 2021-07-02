b426a1bf90c138792385a83783b553aaa46f62a7
minijamigara <55281954+minijamigara@users.noreply.github.com>
2021-07-02 16:39:32 +0530

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,131 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using Android.Views;
+using Android.Runtime;
+using System;
+using Android.Content.PM;
+//using AndroidX.AppCompat.App;
+
+namespace SimpleCalculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.User)]
+    public class MainActivity : Activity
+    {
+        private TextView textView;
+        private string[] numbers = new string[2];
+        private string @operator;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            textView = FindViewById<TextView>(Resource.Id.calculator_text_view);
+        }
+        [Java.Interop.Export("buttonClick")]
+        public void ButtonClick(View v)
+        {
+            Button button = (Button)v;
+            if ("0123456789.".Contains(button.Text))
+                AddDigitOrDecimalPoint(button.Text);
+            else if ("÷×+-%√".Contains(button.Text))
+                AddOperator(button.Text);
+            else if ("=" == button.Text)
+                Calculate();
+            else if ("CEC" == button.Text)
+                Clear();
+            else
+                Erase();
+        }
+
+        private void AddDigitOrDecimalPoint(string value)
+        {
+            int index = @operator == null ? 0 : 1;
+
+            if (value == "." && numbers[index].Contains("."))
+                return;
+
+            numbers[index] += value;
+
+            UpdateCalculatorText();
+        }
+
+        private void AddOperator(string value)
+        {
+            if (numbers[1] != null)
+            {
+                Calculate(value);
+                return;
+            }
+
+            @operator = value;
+
+            UpdateCalculatorText();
+        }
+
+        private void Calculate(string newOperator = null)
+        {
+            double? result = null;
+            double? first = numbers[0] == null ? null : (double?)double.Parse(numbers[0]);
+            double? second = numbers[1] == null ? null : (double?)double.Parse(numbers[1]);
+
+            switch (@operator)
+            {
+                case "÷":
+                    result = first / second;
+                    break;
+                case "×":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+                case "%":
+                    result = (first / 100) * 100 / 100;
+                    break;
+                case "√":
+                    result = Math.Sqrt((double)first);
+                    break;
+            }
+
+            if (result != null)
+            {
+                numbers[0] = result.ToString();
+                @operator = newOperator;
+                numbers[1] = null;
+                UpdateCalculatorText();
+            }
+        }
+
+        private void Clear()
+        {
+            numbers[0] = null;
+            numbers[1] = null;
+            @operator = null;
+            this.textView.Text = "0";
+            UpdateCalculatorText();
+        }
+
+        private void Erase()
+        {
+            numbers[0] = numbers[1] = null;
+            @operator = null;
+            UpdateCalculatorText();
+        }
+        private void UpdateCalculatorText() => textView.Text = $"{numbers[0]} {@operator} {numbers[1]}";
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
+    }
+}
